f08dbf953a887522daddea3fd791a5864053d6ce
Jeffrey <jeffreysoza2000@gmail.com>
2025-01-07 13:05:40 -0600

    Update: moving to angular 18

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: AppComponent }, // Default route
+    { path: 'users', component: UsersComponent },
+    { path: '**', redirectTo: '' }, // Wildcard route for 404
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+})
+export class AppRoutingModule { }
---
+++ src/app/users/users.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from './users.service';
+
+@Component({
+  selector: 'app-users',
+  standalone: true,
+  imports: [],
+  templateUrl: './users.component.html',
+  styleUrl: './users.component.sass'
+})
+export class UsersComponent implements OnInit {
+  users: string[] = [];
+  constructor(private user_service: UsersService) { }
+
+  ngOnInit(): void {
+    this.users = this.user_service.getUsers();
+  }
+}
